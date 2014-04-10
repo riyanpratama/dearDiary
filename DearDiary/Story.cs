288d534d2f172a17eb769258ceba91b34c5097cf
Riyan <riyan.pratama@outlook.com>
2014-04-10 08:16:34 -0700

    second commit

---
+++ DearDiary/Story.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+using System.Data.Linq;
+
+namespace DearDiary
+{
+    [Table]
+    public class Story
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity")]
+        public int idStory { get; set; }
+
+        [Column]
+        public string Title { get; set; }
+
+        [Column]
+        public string Created { get; set; }
+
+        [Column]
+        public string Feeling { get; set; }
+
+        [Column]
+        public string TextStory { get; set; }
+    }
+
+    public class StoryContext : DataContext
+    {
+        public Table<Story> diary;
+        public StoryContext(string connectionstring) : base(connectionstring) { }
+    }
+}
